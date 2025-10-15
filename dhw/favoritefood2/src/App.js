8fdd45cea5d7f461561175193502ac04768a1efb
Keerthirs123 <keerthirs1998@gmail.com>
2025-10-16 00:01:26 +0530

    dhw2 finished

--- dhw/favoritefood2/src/App.js
+++ dhw/favoritefood2/src/App.js
@@ -1,4 +1,23 @@
 import React  from "react";
 function App(){
-  let foods =["Momos","Biriyani","Icecream","Frenchfries"]
+  let foods =["Momos","Biriyani","Icecream","Frenchfries"];
+  let message = "Select a food that you love!"
+  const handleclick = (food) =>{
+    document.getElementById("message").innerText =`I love ${food}!`;
+  };
+  return(
+    <div style={{textAlign:"center", marginTop:"50px"}}>
+      <h2 style={{color: "red"}}>My Favorite Foods</h2>
+      <ul style={{listStyle:"none",padding:0}}>
+        {foods.map((food,index)=> (
+          <li key={index} style={{margin: "10px"}}>
+            {food}{" "}
+            <button style={{color:"red"}} onClick={() => handleclick(food)}>Love it</button>
+          </li>
+        ))}
+      </ul>
+      <h3 id="message" style={{color:"red"}}>{message}</h3>
+    </div>
+  )
 }
+export default App;
